346b3dc95cd085f49f15904f8a955359d98f07f5
AB <alexjacobsonforspam@gmail.com>
2012-02-21 12:20:21 +0700

    Created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whatsinthebox::Application.config.secret_token = '833ea3da6c00e6ac60253e43bd582118ae9c5efeedfccbbd552a40ac1a7552529d9475c5cc4da20861e8cae8e8bc7ad211212188a9d72e4fc8fd4ba8dd380a1e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Whatsinthebox::Application.config.session_store :cookie_store, key: '_whatsinthebox_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Whatsinthebox::Application.config.session_store :active_record_store
